39b78231d3f600601dc0bb03b0f23a15c5fd8bd8
Naman12769 <namanjainjain341@gmail.com>
2025-06-06 14:26:36 +0530

    somthing update

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+  name="wasteDetection",
+  version="0.0.0",
+  author="Naman jain",
+  author_email="naman@gmail.com",
+  packages=find_packages(),
+  install_requires=[]
+)
